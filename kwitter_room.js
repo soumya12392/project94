8753633c073ff72aae60470fba49756618888e01
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2021-08-02 20:24:10 +0530

    Add files via upload
    Completed project 94!

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// My web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDixMNrHCeQlwwkyJjnaCZaqlTFU4633Tk",
+    authDomain: "project-94-ee945.firebaseapp.com",
+    databaseURL: "https://project-94-ee945-default-rtdb.firebaseio.com",
+    projectId: "project-94-ee945",
+    storageBucket: "project-94-ee945.appspot.com",
+    messagingSenderId: "399782941304",
+    appId: "1:399782941304:web:b39a610847d412bfa8138d"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
